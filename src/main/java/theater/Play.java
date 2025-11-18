4217a021616f3bd17c4c56a9256dcbcb329fe012
jayla-12 <jaylazhlee@gmail.com>
2025-11-18 10:21:53 -0500

    Task 1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,44 @@
 package theater;
 
+/**
+ * Represents a play that can be performed.
+ *
+ * @null no parameters; this class holds the play name and type
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Constructs a play with a name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play (e.g., tragedy, comedy)
+     * @null no nullable parameters
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the name of the play.
+     *
+     * @return the play name
+     * @null never returns null
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of the play.
+     *
+     * @return the play type
+     * @null never returns null
+     */
+    public String getType() {
+        return type;
     }
 }
